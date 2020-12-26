4e0b5eed856d0f10afd90975273c80bc39a220e9
AzuriteLab <azurite.cg@gmail.com>
2020-12-26 20:26:07 +0900

    Support Unity2019

--- Editor/VRCHierarchyHighlighter.cs
+++ Editor/VRCHierarchyHighlighter.cs
@@ -38,7 +38,7 @@ using UnityEngine.SceneManagement;
 
 public static class HierarchyIndentHelper
 {
-    public const string kVersion = "2020.11.25.0";
+    public const string kVersion = "2020.12.26.0";
 
     private const string kResourceDirPath = "Assets/VRCHierarchyHighlighter/Editor/Resources/";
     private const string kResourceSuffix = ".png";
@@ -93,6 +93,22 @@ public static class HierarchyIndentHelper
     private static void Startup()
     {
         SetupIcons();
+
+        // Unityのバージョン2019以上になった時のため、またはその逆の判定
+        if (EditorGUIUtility.isProSkin && !VRChierarchyHighlighterEdit.is_dark_mode.GetValue())
+        {
+            Debug.Log("VRCHierarchyHighlighter: Standard Skin -> Pro Skin");
+            // 通常用からダークモードにプリセットに切り替え
+            VRChierarchyHighlighterEdit.SetDefaultAllParameters();
+
+        }
+        else if (!EditorGUIUtility.isProSkin && VRChierarchyHighlighterEdit.is_dark_mode.GetValue())
+        {
+            Debug.Log("VRCHierarchyHighlighter: Pro Skin -> Standard Skin");
+            // ダークモードから通常用のプリセットに切り替える
+            VRChierarchyHighlighterEdit.SetDefaultAllParameters();
+        }
+
         EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindowItemOnGUI;
     }
 
@@ -249,11 +265,11 @@ public static class HierarchyIndentHelper
 
 public struct VHHParameter<T>
 {
-    public VHHParameter(T default_value, string signature, Func<string, T, T> init, Action<string, T> teardown)
+    public VHHParameter(Func<T> set_default_value, string signature, Func<string, T, T> init, Action<string, T> teardown)
     {
-        default_value_ = default_value;
+        do_set_default_ = set_default_value;
         signature_ = signature;
-        value_ = init(signature_, default_value_);
+        value_ = init(signature_, do_set_default_());
         teardown_ = teardown;
     }
 
@@ -263,16 +279,16 @@ public struct VHHParameter<T>
         teardown_(signature_, value_);
     }
 
-    public T GetDefault() { return default_value_; }
+    public T GetDefault() { return do_set_default_(); }
     public T GetValue() { return value_; }
     public string GetSignature() { return signature_; }
 
     public void SetValue(T value) { value_ = value; }
-    public void SetDefault() { value_ = default_value_; }
+    public void SetDefault() { value_ = do_set_default_(); }
 
     private T value_;
-    private T default_value_;
     private string signature_;
+    private Func<T> do_set_default_;
     private Action<string, T> teardown_;
 }
 
@@ -284,28 +300,52 @@ public class VRChierarchyHighlighterEdit : EditorWindow
         GetWindow<VRChierarchyHighlighterEdit>();
     }
 
+    static private Func<T> SetDefault_<T>(T for_std, T for_pro)
+    {
+        return () => EditorGUIUtility.isProSkin ? for_pro : for_std;
+    }
+    static private Func<T> SetDefault_<T>(T value)
+    {
+        return () => value;
+    }
+
     public static VHHParameter<bool> is_draw_icons
-        = new VHHParameter<bool>(true, "vhh.is_draw_icons", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(true), "vhh.is_draw_icons", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<bool> is_draw_highlights
-        = new VHHParameter<bool>(true, "vhh.is_draw_highlights", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(true), "vhh.is_draw_highlights", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<bool> is_draw_vers
-        = new VHHParameter<bool>(false, "vhh.is_draw_vers", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(false), "vhh.is_draw_vers", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<bool> is_underline_mode
-        = new VHHParameter<bool>(false, "vhh.is_underline_mode", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(for_std: false, for_pro: true), "vhh.is_underline_mode", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<bool> is_dark_mode
-        = new VHHParameter<bool>(false, "vhh.is_dark_mode", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(for_std: false, for_pro: true), "vhh.is_dark_mode", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<bool> use_active_checkbox
-        = new VHHParameter<bool>(false, "vhh.use_active_checkbox", EditorPrefs.GetBool, EditorPrefs.SetBool);
+        = new VHHParameter<bool>(SetDefault_(false), "vhh.use_active_checkbox", EditorPrefs.GetBool, EditorPrefs.SetBool);
     public static VHHParameter<float> saturation
-        = new VHHParameter<float>(0.7f, "vhh.saturation", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+        = new VHHParameter<float>(SetDefault_(for_std: 0.7f, for_pro: 0.7f), "vhh.saturation", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
     public static VHHParameter<float> value
-        = new VHHParameter<float>(0.7f, "vhh.value", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+        = new VHHParameter<float>(SetDefault_(for_std: 0.7f, for_pro: 8.5f), "vhh.value", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
     public static VHHParameter<float> hue
-        = new VHHParameter<float>(0.3f, "vhh.hue", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+        = new VHHParameter<float>(SetDefault_(for_std: 0.3f, for_pro: 0.2f), "vhh.hue", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
     public static VHHParameter<float> hue_offset
-        = new VHHParameter<float>(0.2f, "vhh.hue_offset", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+        = new VHHParameter<float>(SetDefault_(for_std: 0.2f, for_pro: 0.2f), "vhh.hue_offset", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
     public static VHHParameter<float> alpha
-        = new VHHParameter<float>(0.2f, "vhh.alpha", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+        = new VHHParameter<float>(SetDefault_(for_std: 0.2f, for_pro: 1.0f), "vhh.alpha", EditorPrefs.GetFloat, EditorPrefs.SetFloat);
+
+    static public void SetDefaultAllParameters()
+    {
+        is_draw_icons.SetDefault();
+        is_draw_highlights.SetDefault();
+        is_draw_vers.SetDefault();
+        is_underline_mode.SetDefault();
+        is_dark_mode.SetDefault();
+        use_active_checkbox.SetDefault();
+        hue_offset.SetDefault();
+        hue.SetDefault();
+        saturation.SetDefault();
+        value.SetDefault();
+        alpha.SetDefault();
+    }
 
     private void OnDestroy()
     {
@@ -328,17 +368,7 @@ public class VRChierarchyHighlighterEdit : EditorWindow
 
         if (GUI.Button(new Rect(EditorGUILayout.GetControlRect().xMax - 100, 10, 100, 20), "Default"))
         {
-            is_draw_icons.SetDefault();
-            is_draw_highlights.SetDefault();
-            is_draw_vers.SetDefault();
-            is_underline_mode.SetDefault();
-            is_dark_mode.SetDefault();
-            use_active_checkbox.SetDefault();
-            hue_offset.SetDefault();
-            hue.SetDefault();
-            saturation.SetDefault();
-            value.SetDefault();
-            alpha.SetDefault();
+            SetDefaultAllParameters();
         }
 
         EditorGUILayout.LabelField("General Settings: ");
@@ -357,14 +387,13 @@ public class VRChierarchyHighlighterEdit : EditorWindow
         EditorGUILayout.LabelField("Highlights Settings: ");
         EditorGUI.indentLevel++;
 
-        hue.SetValue(EditorGUILayout.Slider("Hue", hue.GetValue(), 0.0f, 1.0f));
+        hue.SetValue(EditorGUILayout.Slider("Hue", hue.GetValue(), 0.0f, 2.0f));
         hue_offset.SetValue(EditorGUILayout.Slider("Hue offset", hue_offset.GetValue(), 0.0f, 1.0f));
-        saturation.SetValue(EditorGUILayout.Slider("Saturation", saturation.GetValue(), 0.0f, 1.0f));
-        value.SetValue(EditorGUILayout.Slider("Value", value.GetValue(), 0.0f, 1.0f));
-        alpha.SetValue(EditorGUILayout.Slider("Alpha", alpha.GetValue(), 0.0f, 1.0f));
+        saturation.SetValue(EditorGUILayout.Slider("Saturation", saturation.GetValue(), 0.0f, 2.0f));
+        value.SetValue(EditorGUILayout.Slider("Value", value.GetValue(), 0.0f, 10.0f));
+        alpha.SetValue(EditorGUILayout.Slider("Alpha", alpha.GetValue(), 0.0f, 2.0f));
 
         is_underline_mode.SetValue(EditorGUILayout.ToggleLeft("Underline Mode", is_underline_mode.GetValue()));
-        is_dark_mode.SetValue(EditorGUILayout.ToggleLeft("[Reserve] Dark Mode (Unity Pro, Unity 2019 or later)", is_dark_mode.GetValue()));
 
         EditorGUI.indentLevel--;
 
