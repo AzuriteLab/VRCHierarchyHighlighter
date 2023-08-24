3ec0538739d9fb6088f57c6f1287bab3cc967c9a
anatawa12 <anatawa12@icloud.com>
2023-08-24 19:10:55 +0900

    fix: error with SkinnedMeshRenderer without mesh
    AvatarOptimizer creates SkinnedMeshRenderer without Mesh and it causes error.

--- Editor/VRCHierarchyHighlighter.cs
+++ Editor/VRCHierarchyHighlighter.cs
@@ -316,7 +316,8 @@ public static class HierarchyIndentHelper
         target_rect.height = kIconSize;
 
         var mesh = ((SkinnedMeshRenderer)component).sharedMesh;
-        GUI.Label(target_rect, string.Format("Vers: {0}", mesh.vertexCount));
+        if (mesh)
+            GUI.Label(target_rect, string.Format("Vers: {0}", mesh.vertexCount));
     }
 }
 
